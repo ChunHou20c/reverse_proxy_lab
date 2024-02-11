b4fe42f94a99a3e561c7b16ad5f9f62aa3b11ded
ChunHou20c <youmustbekiddingmeshit@gmail.com>
2024-02-11 16:47:47 +0800

    creating a tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::{convert::Infallible, net::SocketAddr};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = TcpListener::bind(addr).await?;
+
+    Ok(())
+
 }
